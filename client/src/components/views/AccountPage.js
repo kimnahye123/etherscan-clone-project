2e895266c8f96ad4e1e8085101ea0a6e3fa9532f
kimnahye <nahye7670@gmail.com>
2022-10-14 22:46:01 +0900

    수정

--- client/src/components/views/AccountPage.js
+++ client/src/components/views/AccountPage.js
@@ -1,17 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
-//format
-
-// Account detail
-
-// 1. address 가 들어가야합니다.
-//    ID 상태관리, 변하는 값을 req.params에 담에 보내주십쇼.
-
-// 2. 조회한 address 의 Balance 를 가져와야합니다.
-//    axios request로 서버에 호출 보낸 후, result 값을 받아오면, krw로 변경.
-
-// 바로 서버에서 받아오면 되는 부분.
+import { ImCoinDollar } from 'react-icons/im';
 
 function AccountPage() {
   const [address, setAddress] = useState(undefined);
@@ -40,23 +29,39 @@ function AccountPage() {
 
   return (
     <div className="Rectangle-1">
-      <h1 className="ETHERSCAN">ETHERSCAN</h1>
-
-      <input
-        className="Search"
-        type="text"
-        onChange={handleAddress}
-        placeholder="Search by Address"
-      ></input>
-      <button className="button" onClick={handleAddressButton}>
-        click
-      </button>
+      <div>
+        <h2 className="etherscanLogo">
+          EtherScan
+          <img
+            className="etherscanImage"
+            src="/logos/etherscan-logo-circle.png"
+            alt="etsc"
+          />
+        </h2>
+      </div>
+      <div className="SearchParent">
+        <div className="SearchBox">
+          <input
+            className="Search"
+            type="text"
+            onChange={handleAddress}
+            placeholder="Search by Address"
+          ></input>
+        </div>
+        <div className="Vector">
+          <ImCoinDollar
+            size="45"
+            color="white"
+            cursor="pointer"
+            onClick={handleAddressButton}
+          />
+        </div>
+      </div>
       <div className="Rectangle-2">
-        <span className="Balance">
-          Balance :
-          <span className="text-style-1">
-            {balance}ETH {(balance * eth).toLocaleString()}WON
-          </span>
+        Balance:
+        <span className="-ETH">
+          {balance}ETH <br />
+          <p className="-WON">{(balance * eth).toLocaleString()}WON</p>
         </span>
       </div>
     </div>
